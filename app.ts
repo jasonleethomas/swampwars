1ab9c64d5804b7e938b6f067f11567688c0969f2
Alain Galvan <me@alain.xyz>
2016-01-23 18:59:31 -0500

    Remove Jade

--- app.ts
+++ app.ts
@@ -6,13 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-import {index} from './routes/index';
-
 export var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,7 +21,9 @@ app.use('/jspm_packages', express.static(__dirname + '/jspm_packages'));
 app.use('/config.js',function (req, res) {
   res.sendFile(__dirname + '/config.js');
 });
-app.use('/', index);
+app.use('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.ts
+++
@@ -1,7 +0,0 @@
-import * as express from 'express';
-export var index = express.Router();
-
-/* GET home page. */
-index.get('/', function(req, res, next) {
-  res.render('index', { title: 'SwampWars' });
-});
